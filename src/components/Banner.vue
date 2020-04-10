6d30d928556c10463e9d890870e490ef80edd512
elisatheng <elisa.theng@gmail.com>
2020-04-10 23:23:48 +0200

    add banner section

---
+++ src/components/Banner.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-parallax
+        src="@/assets/img/banner-grey.jpg"
+        height="700"
+        class="banner px-app mt-10"
+    >
+        <v-row
+            align="center"
+            justify="center"
+        >
+            <v-col class="mt-9 pa-0" cols="12">
+                <p class="banner-title font-righteous mb-0">Hello, I am Elisa.</p>
+                <p class="banner-subtitle font-righteous">A Web Developer.</p>
+            </v-col>
+        </v-row>
+    </v-parallax>
+</template>
+
+<script>
+    export default {
+        name: 'Banner',
+
+        data: () => ({
+        }),
+    }
+</script>
