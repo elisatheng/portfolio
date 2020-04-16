44035c44e903496091ca02592ccce30a7c82a57a
elisatheng <elisa.theng@gmail.com>
2020-04-16 19:51:00 +0200

    implement card flip in skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,14 +11,13 @@
             </v-col>
             <v-col cols="12" class="skills-content pa-0">
                 <v-row justify="space-around" class="text-center">
-                    <v-avatar
-                        v-for="skill in skills"
-                        :key="skill"
-                        color="black"
-                        size="120"
-                    >
-                        <span>{{ skill }}</span>
-                    </v-avatar>
+                    <flip-card
+                        v-for="(skill, i) in skills"
+                        :key="i"
+                        :frontData="skill.front"
+                        :backData="skill.back"
+                        class="skill-flip mb-3 font-righteous"
+                    ></flip-card>
                 </v-row>
             </v-col>
         </v-row>
@@ -32,15 +31,42 @@
         data: () => ({
             title: 'Skills',
             skills: [
-                'PHP',
-                'Symfony',
-                'HTML',
-                'CSS',
-                'jQuery',
-                'JavaScript',
-                'VueJS',
-                'SQL',
-                'Git',
+                {
+                    front: 'PHP',
+                    back: '<img src="' + require('@/assets/img/skills/php.png') + '" width="100%" alt="PHP logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'Symfony',
+                    back: '<img src="' + require('@/assets/img/skills/symfony.png') + '" width="100%" alt="Symfony logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'HTML',
+                    back: '<img src="' + require('@/assets/img/skills/html.png') + '" width="100%" alt="HTML logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'CSS',
+                    back: '<img src="' + require('@/assets/img/skills/css.png') + '" width="100%" alt="CSS logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'jQuery',
+                    back: '<img src="' + require('@/assets/img/skills/jquery.png') + '" width="100%" alt="jQuery logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'JavaScript',
+                    back: '<img src="' + require('@/assets/img/skills/javascript.png') + '" width="100%" alt="JavaScript logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'VueJS',
+                    back: '<img src="' + require('@/assets/img/skills/vuejs.png') + '" width="100%" alt="VueJS logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'SQL',
+                    back: '<img src="' + require('@/assets/img/skills/mysql.png') + '" width="100%" alt="SQL logo" class="skill-flip-img">',
+                },
+                {
+                    front: 'Git',
+                    back: '<img src="' + require('@/assets/img/skills/git.png') + '" width="100%" alt="Git logo" class="skill-flip-img">',
+                },
             ],
         }),
     }
