1c0e190b91289d0022bda5901de1aec1ba87849f
elisatheng <elisa.theng@gmail.com>
2020-04-16 19:51:00 +0200

    implement card flip in skills

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import InfoCard from 'vue-info-card'
 import '@/assets/css/app.css'
 
 Vue.config.productionTip = false
+
+Vue.component('flip-card', InfoCard)
 
 new Vue({
     vuetify,
