b5e24cc9c4b3a60a6b7702d7637fc1153a049f3b
elisatheng <elisa.theng@gmail.com>
2020-04-17 17:39:02 +0200

    add appbar section

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,30 @@
 <template>
     <v-app>
-        <v-app-bar
-            app
-            color="primary"
-            dark
-        >
-            <div class="d-flex align-center">
-                <v-img
-                    alt="Vuetify Logo"
-                    class="shrink mr-2"
-                    contain
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-                    transition="scale-transition"
-                    width="40"
-                />
+        <v-card class="overflow-hidden">
+            <AppBar/>
 
-                <v-img
-                    alt="Vuetify Name"
-                    class="shrink mt-1 hidden-sm-and-down"
-                    contain
-                    min-width="100"
-                    src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-                    width="100"
-                />
-            </div>
-
-            <v-spacer></v-spacer>
-
-            <v-btn
-                href="https://github.com/vuetifyjs/vuetify/releases/latest"
-                target="_blank"
-                text
+            <v-sheet
+                id="app-content"
+                class="overflow-y-auto mt-12"
+                max-height="1000"
             >
-                <span class="mr-2">Latest Release</span>
-                <v-icon>mdi-open-in-new</v-icon>
-            </v-btn>
-        </v-app-bar>
-
-        <v-content>
-            <HelloWorld/>
-        </v-content>
+                <v-container class="app-container pa-0" fluid></v-container>
+            </v-sheet>
+        </v-card>
     </v-app>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld';
+    import AppBar from './components/AppBar';
 
     export default {
         name: 'App',
 
         components: {
-            HelloWorld,
+            AppBar,
         },
 
         data: () => ({
-            //
         }),
     };
 </script>
