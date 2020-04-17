ee0dcd0935f43a8419d46473acec7fa57407def3
elisatheng <elisa.theng@gmail.com>
2020-04-17 17:39:02 +0200

    add appbar section

---
+++ src/components/AppBar.vue
@@ -0,0 +1,137 @@
+<template>
+    <v-app-bar
+        absolute
+        color="white"
+        shrink-on-scroll
+        prominent
+        scroll-target="#app-content"
+        class="app-bar px-app font-righteous"
+    >
+        <v-row class="h-100">
+            <v-col class="header-navleft text-left h-100 pa-0">
+                <v-list class="d-flex h-100 pa-0">
+                    <v-list-item
+                        v-for="(item, i) in navleftItems"
+                        :key="i"
+                        class="nav-item d-flex align-self-end pa-0"
+                    >
+                        <v-list-item-content class="pa-0">
+                            <a :href="item.href" class="nav-link">{{ item.title }}</a>
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list>
+            </v-col>
+            <v-col cols="2" class="header-profile pa-0">
+                <v-toolbar-title class="pa-0">
+                    <div class="logo">
+                        <v-img
+                            :src="logo.src"
+                            :alt="logo.alt"
+                            :height="logo.size"
+                            :width="logo.size"
+                            class="v-btn--round"
+                        />
+                    </div>
+                </v-toolbar-title>
+            </v-col>
+            <v-col class="header-navright text-right h-100 pa-0">
+                <v-list class="d-flex h-100 pa-0">
+                    <v-list-item
+                        v-for="(item, i) in navrightItems"
+                        :key="i"
+                        class="nav-item d-flex align-self-end pa-0"
+                    >
+                        <v-list-item-content class="pa-0">
+                            <a :href="item.href" class="nav-link">{{ item.title }}</a>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item class="nav-item d-flex align-self-end pa-0">
+                        <v-list class="nav-socials d-flex ml-auto pa-0">
+                            <v-list-item
+                                v-for="(item, i) in socials"
+                                :key="i"
+                                class="nav-item d-flex align-self-end px-1"
+                            >
+                                <v-list-item-content class="pa-0">
+                                    <a target="_blank" :href="item.href" class="nav-link">
+                                        <v-img
+                                            :src="item.img.path"
+                                            :alt="item.img.alt"
+                                            :height="item.img.size"
+                                            :width="item.img.size"
+                                            class="nav-img v-btn--round"
+                                        />
+                                    </a>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </v-list>
+                    </v-list-item>
+                </v-list>
+            </v-col>
+        </v-row>
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        name: 'AppBar',
+
+        data: () => ({
+            logo: {
+                src: require('@/assets/img/logo.jpg'),
+                alt: 'Portfolio logo',
+                size: 100,
+            },
+            navleftItems: [
+                {
+                    title: 'About me',
+                    href: '#aboutme',
+                },
+                {
+                    title: 'Course',
+                    href: '#course',
+                },
+                {
+                    title: 'Skills',
+                    href: '#skills',
+                },
+            ],
+            navrightItems: [
+                {
+                    title: 'Projects',
+                    href: '#projects',
+                },
+                {
+                    title: 'Hobbies',
+                    href: '#hobbies',
+                },
+            ],
+            socials: [
+                {
+                    href: 'https://github.com/elisatheng',
+                    img: {
+                        path: require('@/assets/img/socials/github.png'),
+                        alt: 'Github logo',
+                        size: 35,
+                    },
+                },
+                {
+                    href: 'https://www.linkedin.com/in/elisa-theng/',
+                    img: {
+                        path: require('@/assets/img/socials/linkedin.png'),
+                        alt: 'LinkedIn logo',
+                        size: 35,
+                    },
+                },
+                {
+                    href: 'http://www.viadeo.com/fr/profile/elisatheng',
+                    img: {
+                        path: require('@/assets/img/socials/viadeo.png'),
+                        alt: 'Viadeo logo',
+                        size: 35,
+                    },
+                },
+            ],
+        }),
+    }
+</script>
